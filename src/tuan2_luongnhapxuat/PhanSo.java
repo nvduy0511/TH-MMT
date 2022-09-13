e4c9ac3881c829b50cfa7542bcc2a138d3e38d43
nvduy0511 <nvduy.k19hutech@gmail.com>
2022-09-13 14:25:59 +0700

    13/09 week 2

---
+++ src/tuan2_luongnhapxuat/PhanSo.java
@@ -0,0 +1,41 @@
+
+package tuan2_luongnhapxuat;
+
+import java.io.Serializable;
+
+
+public class PhanSo implements Serializable {
+    private int tuSo;
+    private int mauSo;
+
+    public PhanSo(int tuSo, int mauSo) {
+        this.tuSo = tuSo;
+        this.mauSo = mauSo;
+    }
+
+    public PhanSo() {
+    }
+
+    
+    public int getTuSo() {
+        return tuSo;
+    }
+
+    public void setTuSo(int tuSo) {
+        this.tuSo = tuSo;
+    }
+
+    public int getMauSo() {
+        return mauSo;
+    }
+
+    public void setMauSo(int mauSo) {
+        this.mauSo = mauSo;
+    }
+    
+    @Override
+    public String toString(){
+        return tuSo+" / "+mauSo+"\n";
+    }
+    
+}
---
+++ src/tuan2_luongnhapxuat/Tuan2_LuongNhapXuat.java
@@ -0,0 +1,11 @@
+
+package tuan2_luongnhapxuat;
+
+public class Tuan2_LuongNhapXuat {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    }
+    
+}
